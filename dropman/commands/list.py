d23fc9aa2e7b7973124f44912c6512b25da0f080
Daniel Gomes <me@danielcsgomes.com>
2016-09-05 12:01:41 +0100

    added public ip addresses to the list table

--- dropman/commands/list.py
+++ dropman/commands/list.py
@@ -8,20 +8,31 @@ from .base import Base
 
 class List(Base):
     @staticmethod
-    def output_table(droplets):
-        table = []
+    def aggregate_data(droplets):
+        data = []
+
         for droplet in droplets:
-            table.append(
+            ip_addresses = []
+            for network in droplet.networks:
+                for item in droplet.networks[network]:
+                    if item['type'] == 'public':
+                        ip_addresses.append(item['ip_address'])
+
+            data.append(
                 [
                     droplet.id,
                     droplet.name,
                     droplet.status,
+                    ', '.join(ip_addresses),
                     droplet.region['name'] + ' - ' + droplet.region['slug']
                 ]
             )
+        return data
 
-        print(tabulate(table, headers=["ID", "Name", "Status", "Region"]))
+    @staticmethod
+    def output_table(data):
+        print(tabulate(data, headers=["ID", "Name", "Status", "Ip", "Region"]))
 
     def run(self, api_token):
         manager = digitalocean.Manager(token=api_token)
-        self.output_table(manager.get_all_droplets())
+        self.output_table(self.aggregate_data(manager.get_all_droplets()))
